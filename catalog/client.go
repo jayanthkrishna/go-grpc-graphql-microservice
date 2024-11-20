e97e92a978f585bd07b8064e57cd12cdd06b8e72
jayanthkrishna <jayanth.krishna.g@gmail.com>
2024-11-20 09:23:47 +0530

    Done with catalog service

--- catalog/client.go
+++ catalog/client.go
@@ -1,1 +1,101 @@
 package catalog
+
+import (
+	"context"
+
+	"github.com/jayanthkrishna/go-grpc-graphql-microservice/catalog/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	conn    *grpc.ClientConn
+	service pb.CatalogServiceClient
+}
+
+func NewClient(url string) (*Client, error) {
+	conn, err := grpc.NewClient(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		return nil, err
+	}
+
+	c := pb.NewCatalogServiceClient(conn)
+
+	return &Client{
+		conn:    conn,
+		service: c,
+	}, nil
+}
+
+func (c *Client) Close() {
+	c.conn.Close()
+}
+
+func (c *Client) PostProduct(ctx context.Context, name, description string, price float64) (*Product, error) {
+	r, err := c.service.PostProduct(ctx, &pb.PostProductRequest{
+		Name:        name,
+		Description: description,
+		Price:       price,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Product{
+		Id:          r.Product.Id,
+		Name:        r.Product.Name,
+		Description: r.Product.Description,
+		Price:       r.Product.Price,
+	}, nil
+
+}
+
+func (c *Client) GetProduct(ctx context.Context, id string) (*Product, error) {
+
+	r, err := c.service.GetProduct(ctx, &pb.GetProductRequest{
+		Id: id,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Product{
+		Id:          id,
+		Name:        r.Product.Id,
+		Description: r.Product.Description,
+		Price:       r.Product.Price,
+	}, nil
+
+}
+
+func (c *Client) GetProducts(ctx context.Context, skip uint64, take uint64, ids []string, query string) ([]Product, error) {
+
+	r, err := c.service.GetProducts(ctx, &pb.GetProductsRequest{
+		Skip:  skip,
+		Take:  take,
+		Ids:   ids,
+		Query: query,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	res := []Product{}
+
+	for _, product := range r.Products {
+
+		res = append(res, Product{
+			Id:          product.Id,
+			Name:        product.Name,
+			Description: product.Description,
+			Price:       product.Price,
+		})
+	}
+
+	return res, nil
+
+}
--- catalog/cmd/catalog/main.go
+++ catalog/cmd/catalog/main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/jayanthkrishna/go-grpc-graphql-microservice/catalog"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/tinrab/retry"
+)
+
+type Config struct {
+	DatabaseURL string `envconfig:"DATABASE_URL"`
+}
+
+func main() {
+	var cfg Config
+
+	err := envconfig.Process("", &cfg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var r catalog.Repository
+
+	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
+		r, err = catalog.NewElasticRepository(cfg.DatabaseURL)
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+
+	})
+
+	defer r.Close()
+
+	log.Println("Listening on port 8080")
+
+	s := catalog.NewService(r)
+
+	log.Fatal(catalog.ListenGrpc(s, 8080))
+
+}
