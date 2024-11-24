fe8ac6ecec582b523c83b3baeaea9769872f5e9e
jayanthkrishna <jayanth.krishna.g@gmail.com>
2024-11-24 23:08:50 +0530

    Added DB Logic for orders service

--- order/repository.go
+++ order/repository.go
@@ -12,7 +12,7 @@ type Repository interface {
 
 	PutOrder(ctx context.Context, o Order) error
 
-	GetOrdersForAccount(ctx context.Context, id string) ([]Order, error)
+	GetOrdersForAccount(ctx context.Context, accountId string) ([]Order, error)
 }
 
 type postgresRepository struct {
@@ -67,7 +67,7 @@ func (r *postgresRepository) PutOrder(ctx context.Context, o Order) error {
 	stmt, _ := tx.PrepareContext(ctx, pq.CopyIn("order_products", "order_id", "product_id", "quantity"))
 
 	for _, p := range o.Products {
-		_, err = stmt.ExecContext(ctx, o.Id, p.Id, p.quantity)
+		_, err = stmt.ExecContext(ctx, o.Id, p.Id, p.Quantity)
 
 		if err != nil {
 			return err
@@ -84,6 +84,80 @@ func (r *postgresRepository) PutOrder(ctx context.Context, o Order) error {
 
 }
 
-func (r *postgresRepository) GetOrdersForAccount(ctx context.Context, id string) ([]Order, error) {
+func (r *postgresRepository) GetOrdersForAccount(ctx context.Context, accountId string) ([]Order, error) {
+	rows, err := r.db.QueryContext(ctx,
+		`SELECT o.id, o.create_at,o.account_id,o.total_price::money::numeric::float8
+	op.product_id,
+      op.quantity
+    FROM orders o JOIN order_products op ON (o.id = op.order_id)
+    WHERE o.account_id = $1
+    ORDER BY o.id
+	
+	`, accountId,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	orders := []Order{}
+	order := &Order{}
+	lastOrder := &Order{}
+
+	orderedProduct := &OrderedProduct{}
+
+	products := []OrderedProduct{}
+
+	for rows.Next() {
+		if err = rows.Scan(
+			&order.Id,
+			&order.CreatedAt,
+			&order.AccountId,
+			&order.TotalPrice,
+			&orderedProduct.Id,
+			&orderedProduct.Quantity,
+		); err != nil {
+			return nil, err
+		}
+		// Scan order
+		if lastOrder.Id != "" && lastOrder.Id != order.Id {
+			newOrder := Order{
+				Id:         lastOrder.Id,
+				AccountId:  lastOrder.AccountId,
+				CreatedAt:  lastOrder.CreatedAt,
+				TotalPrice: lastOrder.TotalPrice,
+				Products:   products,
+			}
+			orders = append(orders, newOrder)
+			products = []OrderedProduct{}
+		}
+		// Scan products
+		products = append(products, OrderedProduct{
+			Id:       orderedProduct.Id,
+			Quantity: orderedProduct.Quantity,
+		})
+
+		*lastOrder = *order
+	}
+
+	// Add last order (or first :D)
+	if lastOrder != nil {
+		newOrder := Order{
+			Id:         lastOrder.Id,
+			AccountId:  lastOrder.AccountId,
+			CreatedAt:  lastOrder.CreatedAt,
+			TotalPrice: lastOrder.TotalPrice,
+			Products:   products,
+		}
+		orders = append(orders, newOrder)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return orders, nil
 
 }
--- order/service.go
+++ order/service.go
@@ -1,10 +1,13 @@
 package order
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Service interface {
-	PostOrder()
-	GetOrdersForAccount()
+	PostOrder(ctx context.Context, accountID string, products []OrderedProduct) (*Order, error)
+	GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error)
 }
 type Order struct {
 	Id         string
@@ -15,4 +18,24 @@ type Order struct {
 }
 
 type OrderedProduct struct {
+	Id          string
+	Name        string
+	Description string
+	Price       float64
+	Quantity    uint32
+}
+
+type orderService struct {
+	r Repository
+}
+
+func NewService(r Repository) Service {
+	return &orderService{r}
+}
+
+func (s *orderService) PostOrder(ctx context.Context, accountID string, products []OrderedProduct) (*Order, error) {
+
+}
+func (s *orderService) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
+
 }
